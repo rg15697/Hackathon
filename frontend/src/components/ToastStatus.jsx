24973d290b538b8383d719b39509e16f8a15e143
Rohit gupta <rg15697@gmail.com>
2022-09-01 04:38:41 +0530

    signin-signup

---
+++ frontend/src/components/ToastStatus.jsx
@@ -0,0 +1,26 @@
+import { Button, useToast, Wrap, WrapItem } from '@chakra-ui/react'
+
+export function ToastStatus({status}) {
+    const toast = useToast()
+    const statuses = [status]
+  
+    return (
+      <Wrap>
+        {statuses.map((status, i) => (
+          <WrapItem key={i}>
+            <Button
+              onClick={() =>
+                toast({
+                  title: `${status} toast`,
+                  status: status,
+                  isClosable: true,
+                })
+              }
+            >
+              Show {status} toast
+            </Button>
+          </WrapItem>
+        ))}
+      </Wrap>
+    )
+  }
---
+++ frontend/src/redux/auth/action.js
@@ -0,0 +1,58 @@
+import React from "react"
+import axios from "axios"
+import {
+    SIGNUP_REQUEST,
+    SIGNUP_SUCCESS,
+    SIGNUP_FAILURE,
+    SIGNIN_REQUEST,
+    SIGNIN_SUCCESS,
+    SIGNIN_FAILURE
+} from "./actionType"
+
+export const signupRequest = () =>{
+    return {
+        type : SIGNUP_REQUEST
+    }
+}
+export const signupSuccess = (payload) =>{
+    return {
+        type : SIGNUP_SUCCESS,
+        payload
+    }
+}
+export const signupFailure = () =>{
+    return {
+        type : SIGNUP_FAILURE
+    }
+}
+
+export const signinRequest = () =>{
+    return {
+        type : SIGNIN_REQUEST
+    }
+}
+export const signinSuccess = (payload) =>{
+    return {
+        type : SIGNIN_SUCCESS,
+        payload
+    }
+}
+export const signinFailure = () =>{
+    return {
+        type : SIGNIN_FAILURE
+    }
+}
+
+export const signupUser = (payload) => (dispatch) => {
+    dispatch(signupRequest())
+    return axios.post("",payload)
+    .then((res)=>dispatch(signupSuccess(res.data)))
+    .catch((err)=>dispatch(signupFailure(err)))
+}
+
+export const signinUser = (payload) => (dispatch) => {
+    dispatch(signinRequest())
+    return axios.post("",payload)
+    .then((res)=>dispatch(signinSuccess(res.data)))
+    .catch((err)=>dispatch(signinFailure(err)))
+}
