7d66dab84712c81e12429248f75472addc05c5bb
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-09-03 02:08:29 +0530

    xyz

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Server } = require("socket.io");
 const userRoutes = require("./routes/User.routes");
 const connection = require("./config/db");
 const app = express();
@@ -8,12 +9,48 @@ app.use(express.json());
 
 app.use("/user", userRoutes);
 
+const webServer = require("http").createServer(app);
 
+const wss = new Server(webServer);
 
+const history = [];
 
-webServer.listen(process.env.PORT, async () => {
-  await connection;
-  console.log(`Server started on port ${process.env.PORT}`);
+wss.on("connection", (ws) => {
+  // every single socket except current socket
+  ws.broadcast.emit("new user", history);
+  ws.emit("history", history);
+});
+
+// CODE TO SHOW "OTHER USER IS TYPING"
+// IT REEPLACES THE OTHER MESSAGES HISTORY
+// ONLY FOR UNDERSTANDING PURPOSE
+// wss.on("connection", (ws) => {
+//   ws.on("typing", () => {
+//     ws.broadcast.emit("typing");
+//   });
+// });
+
+wss.on("connection", (ws) => {
+  ws.on("new message", (m) => {
+    history.push(m);
+    // use ws to get the message on all the users except the sender user
+    // ws.broadcast.emit("new message", m);
+
+    // use wss to get the message on all the users including itself
+    wss.emit("new message", m);
+  });
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// sendFile is express operation, not OS- file/fs operation
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+webServer.listen(8080, () => {
+  console.log("Server started on port 8080");
 });
 
 // app.listen(process.env.PORT, async () => {
